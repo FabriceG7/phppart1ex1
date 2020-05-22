96c849da868ba39a409aff7ca1daa0e36fa1cc6e
FabriceG7 <goret.goret@sfr.fr>
2020-05-22 12:13:55 +0200

    phppart1ex1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$object = 'tu es là ?';
+$message = 'a tout à l\'heure';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>part1 php ex1</title>
+  </head>
+  <body>
+  <h1>exercice 1</h1>
+  <p><?= $object; ?></p>
+  <p><?= $message; ?></p>
+  </body>
+</html>
